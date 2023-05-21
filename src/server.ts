5c044832fe1ba308339b751b781405a5242b9ab4
Mateus Brandão <mateusfleria@gmail.com>
2023-05-20 23:01:48 -0300

    Rota upload de arquivos, utilizando stream, e criação de rota estática

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,20 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
 
+app.register(multipart)
+
+app.register(require('@fastify/static'), { 
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads'
+})
 app.register(cors,{ origin: true })
 
 if(!process.env.JWT_SECRET_KEY) throw new Error(
@@ -19,6 +28,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app.listen({
